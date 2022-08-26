781866ffa044f91b3dcf8ed302b59edecccc1687
DomagojM95 <81382671+DomagojM95@users.noreply.github.com>
2022-08-26 18:59:11 +0200

    Add files via upload

--- fundamentals-part-2.js
+++ fundamentals-part-2.js
@@ -10,4 +10,24 @@ if (hasDriversLicence) console.log("i can drive");
 
 /////////////////////////////////////////////////
 
+*/
+/////////////// functions ////////////////////
 
+function logger() {
+  console.log("my name is domagoj");
+}
+
+logger();
+
+function fruitprocessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitprocessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitprocessor(2, 4);
+
+console.log(appleOrangeJuice);
+/////////////////////////////////////////////////////////////////////////////////////////////
